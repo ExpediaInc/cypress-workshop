32f0c1034207d4c06fad7da8779d55e065d46478
fmccready <fmccready@gmail.com>
2020-11-17 12:40:14 -0600

    rename test

--- cypress/integration/workshop.ts
+++ cypress/integration/workshop.ts
@@ -16,7 +16,7 @@ describe('Search Results Page', () => {
     });
 
     describe('Search Results', () => {
-        it('should display COVID-19 banner on page load', () => {
+        it('should display search results section', () => {
             // Test code goes here
         });
     });
