b47a2e6e8c0880168e5182fb983bec60aa543fb1
dadajian <v-dadajian@expediagroup.com>
2020-11-17 10:34:42 -0600

    initial setup

---
+++ cypress/integration/workshop.ts
@@ -0,0 +1,28 @@
+const url = '/Hotel-Search?adults=2&destination=Frankfurt%20%28and%20vicinity%29%2C%20Hessen%2C%20Germany&endDate=2020-12-03&regionId=179894&semdtl=&sort=RECOMMENDED&startDate=2020-12-01&theme=&useRewards=true&userIntent&x_pwa=1';
+
+describe('Search Results Page', () => {
+    before(() => {
+        cy.visit(url);
+    });
+
+    describe('COVID Banner', () => {
+        it('should display COVID-19 banner on page load', () => {
+            cy.findByText('Germany may have travel restrictions in place due to COVID-19.')
+                .should('be.visible');
+        });
+
+        it('can dismiss banner', () => {
+            cy.findByRole('button', { name: /Dismiss/ })
+                .click()
+                .findByText('Germany may have travel restrictions in place due to COVID-19.')
+                .should('not.exist');
+        });
+    });
+
+    describe('Search Results', () => {
+        it('should display COVID-19 banner on page load', () => {
+            cy.get('section[data-stid="section-results"]')
+                .should('be.visible');
+        });
+    });
+});
---
+++ cypress/plugins/index.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+module.exports = function (on, config) {
+    // configure plugins here
+}
